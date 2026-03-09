39c15405c7928547cdd76ec369141ff032afa8e2
basde <bas.dendekker@edu-kw1c.nl>
2026-03-09 12:40:17 +0100

    een pagina gemaakt met de pokedex door middel van een datebase

--- pokedex/index.php
+++ pokedex/index.php
@@ -4,29 +4,84 @@
  * Date: 06-03-2026
  * pokedex indexpagina
  * */
+?>
 
-//includen van de db_functions map
-include "../includes/db_functions.php";
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="styles/style.css">
+    <title>Pokedex</title>
+</head>
 
-//datebase connection aanmaken
-startConnection("pokemondb");
+<body>
+    <header>
+        <h1>
+            De Pokédex
+        </h1>
+    </header>
 
-//query voor alle date uit de datebase
-//$query = "SELECT * FROM pokemon; WHERE type1 = 'fire' OR type1 = 'water'";
-$query = "SELECT * fROM pokemon;";
-//query uitvoeren
-$result = ExecuteSelectQuery($query);
+    <main>
+        <form action="index.php" method="GET">
+            <fieldset>
+                <legend>Zoeken</legend>
+               <!-- <label>Zoeken</label> -->
+                <input type="text" name="searchName">
 
-//Lopen door elke rij en per rij gegeven tonen
-foreach($result as $row)
-{
+                <input type="submit"  name="searchForm" value="Zoeken...">
 
-    $img = $row["picture"];
-    $name = $row["name"];
+            </fieldset>
+        </form>
+        <article id="overview">
+            <?php
+            //includen van de db_functions map
+            include "../includes/db_functions.php";
 
-    echo "<article>";
-    echo $name;
-    echo "<img src='$img' alt='$name' width='50'>";
-    echo "</article>";
-}
+            //datebase connection aanmaken
+            startConnection("pokemondb");
 
+            //query voor alle date uit de datebase
+            //$query = "SELECT * FROM pokemon; WHERE type1 = 'fire' OR type1 = 'water'";
+
+            //code om het zoek veld te laten functioneren
+            if(isset($_GET["searchForm"]))
+            {
+                $searchName = $_GET["searchName"];
+
+                $query = "SELECT * FROM pokemon WHERE name LIKE '%$searchName%';";
+
+                echo "U heeft gezocht op $searchName";
+            }
+            else
+            {
+                $query = "SELECT * fROM pokemon;";
+            }
+
+            //query uitvoeren
+            $result = ExecuteSelectQuery($query);
+
+            //Lopen door elke rij en per rij gegeven tonen
+            foreach($result as $row)
+            {
+
+                $img = $row["picture"];
+                $name = $row["name"];
+
+                echo "<article>";
+                echo $name;
+                echo "<img src='$img' alt='$name' width='50'>";
+                echo "</article>";
+            }
+            ?>
+        </article>
+    </main>
+
+    <footer>
+
+    </footer>
+
+</body>
+</html>
